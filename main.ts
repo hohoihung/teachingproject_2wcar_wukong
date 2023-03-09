2ddb7e10499439d2b1fa649e77f361c2111d67d6
hohoihung <w10546.ho@gmail.com>
2023-03-09 14:38:53 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+radio.setGroup(8)
+wuKong.setAllMotor(0, 0)
+let _inMotion = 0
+let _maxSpeed = 100
+let _speedThreshold = 35
+let _obsDistance = 40
+let _fallDistance = 3
+let _collide = 0
+let _fall = 0
+basic.forever(function () {
+	
+})
