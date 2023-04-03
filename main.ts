fa704ab269a0ac0dca9e6f5084b46a8db96b3add
hohoihung <w10546.ho@gmail.com>
2023-04-03 15:13:09 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,6 +6,8 @@ function moveReverse () {
     }
     wuKong.stopAllMotor()
     wuKong.setAllMotor(0 - _speed, _speed)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(1000)
 }
 function moveRight () {
     if (_speed <= _speedThreshold) {
@@ -15,6 +17,8 @@ function moveRight () {
     }
     wuKong.stopAllMotor()
     wuKong.setAllMotor(_speed, 0)
+    basic.showArrow(ArrowNames.East)
+    basic.pause(1000)
 }
 function moveForward () {
     if (_speed <= _speedThreshold) {
@@ -24,17 +28,18 @@ function moveForward () {
     }
     wuKong.stopAllMotor()
     wuKong.setAllMotor(_speed, 0 - _speed)
-    _inMotion = 1
+    basic.showArrow(ArrowNames.North)
+    basic.pause(1000)
 }
 function avoid_collision () {
     moveReverse()
-    basic.pause(randint(400, 800))
+    basic.pause(randint(200, 500))
     if (Math.randomBoolean()) {
         reverseLeft()
     } else {
         reverseRight()
     }
-    basic.pause(randint(300, 1000))
+    basic.pause(randint(300, 900))
     _collide = 0
     basic.clearScreen()
 }
@@ -44,6 +49,7 @@ input.onButtonPressed(Button.A, function () {
 function stopMoving () {
     wuKong.stopAllMotor()
     _inMotion = 0
+    basic.showIcon(IconNames.No)
 }
 function reverseLeft () {
     if (_speed <= _speedThreshold) {
@@ -53,9 +59,25 @@ function reverseLeft () {
     }
     wuKong.stopAllMotor()
     wuKong.setAllMotor(0 - _speed, 0)
+    basic.showArrow(ArrowNames.SouthEast)
+    basic.pause(1000)
+}
+function avoidRC () {
+    wuKong.stopAllMotor()
+    _reverseCollide = 0
+    _inMotion = 0
 }
 input.onButtonPressed(Button.AB, function () {
-    stopMoving()
+    wuKong.stopAllMotor()
+    _pin12_high = 10
+    serial.redirectToUSB()
+    serial.writeValue("_pin12", _pin12)
+    _inMotion = 0
+    serial.redirect(
+    SerialPin.P2,
+    SerialPin.P1,
+    BaudRate.BaudRate115200
+    )
 })
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "forward") {
@@ -85,10 +107,10 @@ radio.onReceivedString(function (receivedString) {
     }
 })
 input.onButtonPressed(Button.B, function () {
-    wuKong.stopAllMotor()
-    _pin12_high = 10
-    serial.writeValue("_pin12", _pin12)
-    _inMotion = 0
+    stopMoving()
+    serial.writeString("Hello")
+    basic.pause(2000)
+    serial.writeString("World")
 })
 function reverseRight () {
     if (_speed <= _speedThreshold) {
@@ -98,14 +120,9 @@ function reverseRight () {
     }
     wuKong.stopAllMotor()
     wuKong.setAllMotor(0, _speed)
+    basic.showArrow(ArrowNames.SouthWest)
+    basic.pause(1000)
 }
-input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    serial.writeValue("_dist1", _dist1)
-    serial.writeValue("_collide", _collide)
-    serial.writeValue("_inMotion", _inMotion)
-    serial.writeValue("Obstacle distance", _obsDistance)
-    serial.writeValue("_speed", _speed)
-})
 function moveLeft () {
     if (_speed <= _speedThreshold) {
         _speed = _speedThreshold
@@ -114,18 +131,22 @@ function moveLeft () {
     }
     wuKong.stopAllMotor()
     wuKong.setAllMotor(0, 0 - _speed)
+    basic.showArrow(ArrowNames.South)
+    basic.pause(1000)
 }
 function avoid_fall () {
     avoid_collision()
     _fall = 0
 }
+let uart_rx = ""
+let _pin8 = 0
 let _pin13 = 0
 let _dist1 = 0
 let _pin12 = 0
 let _pin12_high = 0
+let _reverseCollide = 0
 let _fall = 0
 let _collide = 0
-let _obsDistance = 0
 let _speedThreshold = 0
 let _maxSpeed = 0
 let _inMotion = 0
@@ -136,15 +157,20 @@ _speed = 45
 _inMotion = 0
 _maxSpeed = 100
 _speedThreshold = 45
-_obsDistance = 12
+let _obsDistance = 12
 _collide = 0
 _fall = 0
 let _temp = 0
+_reverseCollide = 0
 pins.setPull(DigitalPin.P13, PinPullMode.PullDown)
 pins.setPull(DigitalPin.P12, PinPullMode.PullDown)
-pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 _pin12_high = 1
-pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
+pins.setPull(DigitalPin.P8, PinPullMode.PullDown)
+serial.redirect(
+SerialPin.P2,
+SerialPin.P1,
+BaudRate.BaudRate115200
+)
 basic.showIcon(IconNames.SmallHeart)
 basic.forever(function () {
     _dist1 = sonar.ping(
@@ -157,7 +183,14 @@ basic.forever(function () {
     }
     _pin13 = pins.digitalReadPin(DigitalPin.P13)
     _pin12 = pins.digitalReadPin(DigitalPin.P12)
-    if (_pin12 == _pin12_high) {
+    _pin8 = pins.digitalReadPin(DigitalPin.P8)
+    if (_pin8 == 1) {
+        _reverseCollide = 1
+    }
+    if (_reverseCollide == 1) {
+        avoidRC()
+    }
+    if (_inMotion && _pin12 == _pin12_high) {
         wuKong.stopAllMotor()
         basic.showIcon(IconNames.LeftTriangle)
         avoid_fall()
@@ -167,7 +200,29 @@ basic.forever(function () {
         basic.showIcon(IconNames.No)
         avoid_collision()
     }
+    uart_rx = serial.readString()
+    basic.showString(uart_rx)
+    if (uart_rx == "forward") {
+        basic.showArrow(ArrowNames.North)
+    } else if (uart_rx == "reverse") {
+        basic.showArrow(ArrowNames.South)
+    } else if (uart_rx == "left") {
+        basic.showArrow(ArrowNames.West)
+    } else if (uart_rx == "right") {
+        basic.showArrow(ArrowNames.East)
+    } else if (uart_rx == "reverseLeft") {
+        basic.showArrow(ArrowNames.SouthWest)
+    } else if (uart_rx == "reverseRight") {
+        basic.showArrow(ArrowNames.SouthEast)
+    } else if (uart_rx == "stop") {
+        basic.showIcon(IconNames.No)
+    } else if (uart_rx == "moveForward") {
+        moveForward()
+        basic.showIcon(IconNames.EigthNote)
+    }
     if (_inMotion) {
         moveForward()
+    } else {
+        stopMoving()
     }
 })
