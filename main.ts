7823369e8741f557643e89e4c5f59f10f588617c
hohoihung <w10546.ho@gmail.com>
2023-05-03 15:42:54 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,6 @@
 function moveReverse () {
-    if (_speed <= _speedThreshold) {
-        _speed = _speedThreshold
-    } else if (_speed >= _speedThreshold) {
-        _speed = _maxSpeed
-    }
     wuKong.stopAllMotor()
-    wuKong.setAllMotor(0 - _speed, _speed)
+    wuKong.setAllMotor(0 - _speedThreshold, _speedThreshold)
     basic.showArrow(ArrowNames.South)
     if (_demoMode == 1) {
     	
@@ -33,7 +28,7 @@ function moveRight () {
 function moveForward () {
     if (_speed <= _speedThreshold) {
         _speed = _speedThreshold
-    } else if (_speed >= _speedThreshold) {
+    } else if (_speed >= _maxSpeed) {
         _speed = _maxSpeed
     }
     wuKong.stopAllMotor()
@@ -48,13 +43,13 @@ function moveForward () {
 }
 function avoid_collision () {
     moveReverse()
-    basic.pause(randint(200, 500))
+    basic.pause(randint(100, 300))
     if (Math.randomBoolean()) {
         reverseLeft()
     } else {
         reverseRight()
     }
-    basic.pause(randint(300, 900))
+    basic.pause(randint(100, 400))
     basic.clearScreen()
     moveForward()
 }
@@ -68,13 +63,8 @@ function stopMoving () {
     basic.showIcon(IconNames.No)
 }
 function reverseLeft () {
-    if (_speed <= _speedThreshold) {
-        _speed = _speedThreshold
-    } else if (_speed >= _speedThreshold) {
-        _speed = _maxSpeed
-    }
     wuKong.stopAllMotor()
-    wuKong.setAllMotor(0 - _speed, 0)
+    wuKong.setAllMotor(0 - _speedThreshold, 0)
     basic.showArrow(ArrowNames.SouthEast)
     if (_demoMode == 1) {
     	
@@ -83,18 +73,6 @@ function reverseLeft () {
         wuKong.stopAllMotor()
     }
 }
-input.onButtonPressed(Button.AB, function () {
-    wuKong.stopAllMotor()
-    _pin12_high = 10
-    serial.redirectToUSB()
-    serial.writeValue("_pin12", _pin12)
-    _demoMode = 0
-    serial.redirect(
-    SerialPin.P2,
-    SerialPin.P1,
-    BaudRate.BaudRate115200
-    )
-})
 radio.onReceivedString(function (receivedString) {
     if (receivedString == "forward") {
         moveForward()
@@ -116,12 +94,6 @@ radio.onReceivedString(function (receivedString) {
     } else {
     	
     }
-})
-input.onButtonPressed(Button.B, function () {
-    stopMoving()
-    serial.writeString("Hello")
-    basic.pause(2000)
-    serial.writeString("World")
 })
 serial.onDataReceived(serial.delimiters(Delimiters.Hash), function () {
     uart_rx = serial.readUntil(serial.delimiters(Delimiters.Hash))
@@ -145,13 +117,8 @@ serial.onDataReceived(serial.delimiters(Delimiters.Hash), function () {
     }
 })
 function reverseRight () {
-    if (_speed <= _speedThreshold) {
-        _speed = _speedThreshold
-    } else if (_speed >= _speedThreshold) {
-        _speed = _maxSpeed
-    }
     wuKong.stopAllMotor()
-    wuKong.setAllMotor(0, _speed)
+    wuKong.setAllMotor(0, _speedThreshold)
     basic.showArrow(ArrowNames.SouthWest)
     if (_demoMode == 1) {
     	
@@ -161,13 +128,8 @@ function reverseRight () {
     }
 }
 function moveLeft () {
-    if (_speed <= _speedThreshold) {
-        _speed = _speedThreshold
-    } else if (_speed >= _speedThreshold) {
-        _speed = _maxSpeed
-    }
     wuKong.stopAllMotor()
-    wuKong.setAllMotor(0, 0 - _speed)
+    wuKong.setAllMotor(0, 0 - _speedThreshold)
     basic.showArrow(ArrowNames.South)
     if (_demoMode == 1) {
     	
@@ -181,20 +143,21 @@ function avoid_fall () {
 }
 let _dist1 = 0
 let _pin8 = 0
-let _pin13 = 0
+let _intensity = 0
 let uart_rx = ""
-let _pin12 = 0
-let _pin12_high = 0
+let _speed = 0
 let _speedThreshold = 0
 let _maxSpeed = 0
 let _demoMode = 0
-let _speed = 0
 radio.setGroup(8)
 wuKong.stopAllMotor()
-_speed = 50
+let _gain = 1
+let _gap = 100
+let _max_intensity = 60
+let _min_intensity = 20
 _demoMode = 0
 _maxSpeed = 100
-_speedThreshold = 50
+_speedThreshold = 35
 let _obsDistance = 12
 let _collide = 0
 let _fall = 0
@@ -202,31 +165,15 @@ let _temp = 0
 let _reverseCollide = 0
 pins.setPull(DigitalPin.P13, PinPullMode.PullDown)
 pins.setPull(DigitalPin.P12, PinPullMode.PullDown)
-_pin12_high = 1
 pins.setPull(DigitalPin.P8, PinPullMode.PullDown)
-serial.redirect(
-SerialPin.P2,
-SerialPin.P1,
-BaudRate.BaudRate115200
-)
 basic.showIcon(IconNames.SmallHeart)
 basic.forever(function () {
-    _pin13 = pins.digitalReadPin(DigitalPin.P13)
-    if (_pin13 == 1) {
-        wuKong.stopAllMotor()
-        basic.showIcon(IconNames.Square)
-        if (_demoMode == 1) {
-            avoid_fall()
-        }
-    }
-    _pin12 = pins.digitalReadPin(DigitalPin.P12)
-    if (_pin12 == 1) {
-        wuKong.stopAllMotor()
-        basic.showIcon(IconNames.LeftTriangle)
-        if (_demoMode == 1) {
-            avoid_fall()
-        }
-    }
+    serial.writeValue("_pin0", pins.analogReadPin(AnalogPin.P0))
+    _intensity = Math.map(pins.analogReadPin(AnalogPin.P0), _min_intensity, _max_intensity, 1, 100)
+    _gap = Math.abs(_intensity - 50)
+    _speed = _gain * (_speedThreshold + _gap)
+    serial.writeValue("_speed", _speed)
+    serial.writeValue("_intensity", _intensity)
     _pin8 = pins.digitalReadPin(DigitalPin.P8)
     if (_pin8 == 1) {
         wuKong.stopAllMotor()
